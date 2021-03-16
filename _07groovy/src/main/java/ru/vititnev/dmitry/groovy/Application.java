bd5bca7ad78ad198e32e40396bfa6ed0ec96a41f
Dmitry Vitutnev <dimason99@gmail.com>
2021-03-16 08:36:23 +0600

    Task 7 done

---
+++ _07groovy/src/main/java/ru/vititnev/dmitry/groovy/Application.java
@@ -0,0 +1,11 @@
+package ru.vititnev.dmitry.groovy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ _07groovy/src/main/java/ru/vititnev/dmitry/groovy/controller/MainController.java
@@ -0,0 +1,30 @@
+package ru.vititnev.dmitry.groovy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.vititnev.dmitry.groovy.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MainController {
+    private static List<Person> persons = new ArrayList<Person>();
+
+    static {
+        persons.add(new Person("Bill", "Gates"));
+        persons.add(new Person("Steve", "Jobs"));
+    }
+
+    @RequestMapping(value = "/")
+    public String handleRequest(Model model) {
+
+        String message = "Person List:";
+
+        model.addAttribute("message", message);
+        model.addAttribute("persons", persons);
+
+        return "index";
+    }
+}
