43f4b72458e64b6437c3ce478b42ca018346ba0d
Dmitry Vitutnev <dimason99@gmail.com>
2021-03-16 09:27:52 +0600

    Task 9 done

---
+++ _09jsp/src/main/java/ru/vitutnev/dmitry/jsp/Application.java
@@ -0,0 +1,11 @@
+package ru.vitutnev.dmitry.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
